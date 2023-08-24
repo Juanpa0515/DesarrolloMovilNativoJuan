1401ab5652e5090b755e9f58f81fadd159d787c9
GG-Afterdeath <maicoljosue@outlook.com>
2023-08-24 17:22:59 -0500

    Subo avanse del proyecto de desarrollo movil nativo

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "App2687340_juan_pablo_suarez_rincon"
+include(":app")
+ 
